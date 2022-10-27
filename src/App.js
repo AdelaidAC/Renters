3d6fd44ebb2b60f76e6fdceab55255d5c7f8e152
Adelaid Acevedo Cardona <adelaid.acevedo16@tectijuana.edu.mx>
2022-10-27 15:00:59 -0700

    update 1

--- src/App.js
+++ src/App.js
@@ -3,36 +3,15 @@ import { Button } from 'react-bootstrap';
 import './App.css';
 import ReactToPrint from 'react-to-print';
 
-import Bond from './components/BondInternals/Bond';
-import GeneralLiability from './components/GeneralLiability/GeneralLiability';
-import Flood from './components/FloodInternals/Flood';
 import Home from './components/HomeInternals/Home';
-import Auto from './components/AutoInternals/Auto';
-import Trucking from './components/TruckingInternals/Trucking';
-import Workers from './components/WorkersComp/Workers';
-import SpecialEvent from './components/SpecialEvent/SpecialEvent';
-import GarageLiabilityIS from './components/GarageLiability/GarageLiabilityIS';
+
 import CurrencyFormat from 'react-currency-format';
 import moment from 'moment';
 
 
 const Page = React.forwardRef((
   {
-    bond, 
-    commercialAuto, 
-    flood, 
-    generalLiability, 
-    home, 
-    trucking, 
-    workersComp, 
-    specialEvent,
-    garageLiability,
-    pageGL, 
-    pageT, 
-    pageWC, 
-    pageH, 
-    pageA, 
-    pageB, 
+    pageH,
     nameI,
     addressI,
     phoneI,
@@ -43,15 +22,7 @@ const Page = React.forwardRef((
       
       <style>{getPageMargins()}</style>
 
-      {bond && <Bond pages={pageB} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {commercialAuto && <Auto pages={pageA} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {flood && <Flood name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {generalLiability && <GeneralLiability pages={pageGL} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {home && <Home pages={pageH} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {trucking && <Trucking pages={pageT} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {workersComp && <Workers pages={pageWC} name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {specialEvent && <SpecialEvent name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
-      {garageLiability && <GarageLiabilityIS name={nameI} address={addressI} phone={phoneI} date={dateI}/>}
+      <Home pages={pageH} name={nameI} address={addressI} phone={phoneI} date={dateI}/>
       
     </div>
   )
@@ -67,57 +38,14 @@ const getPageMargins = () => {
 };
 
 function App() {
-  
-  /* ELEGIR DOCUMENTO */
-  const [document, setDocument] = useState("selectDocument");
-
-  const [bondVisible, setBondVisible] = useState(false);
-  const [commercialAutoVisible, setCommercialAutoVisible] = useState(false);
-  const [floodVisible, setFloodVisible] = useState(false);
-  const [generalLiabilityVisible, setGeneralLiabilityVisible] = useState(false);
-  const [homeVisible, setHomeVisible] = useState(false);
-  const [truckingVisible, setTruckingVisible] = useState(false);
-  const [workersCompVisible, setWorkersCompVisible] = useState(false);
-  const [specialEventVisible, setSpecialEventVisible] = useState(false);
-  const [garageLiabilityVisible, setGarageLiabilityVisible] = useState(false);
-
-  const handleRadioButton = (e) => {
-    setDocument(e.target.value);
-  }
-
 
   /* ELEGIR PAGINAS */
-
-  const [pageGLInfo, setPageGLInfo] = useState({
-    pagesGL: []
-  });
-
-  const [pageTInfo, setPageTInfo] = useState({
-    pagesT: []
-  });
-
-  const [pageWCInfo, setPageWCInfo] = useState({
-    pagesWC: []
-  });
 
   const [pageHInfo, setPageHInfo] = useState({
     pagesH: []
   });
 
-  const [pageAInfo, setPageAInfo] = useState({
-    pagesA: []
-  });
-
-  const [pageBInfo, setPageBInfo] = useState({
-    pagesB: []
-  });
-
-  const { pagesGL } = pageGLInfo;
-  const { pagesT } = pageTInfo;
-  const { pagesWC } = pageWCInfo;
   const { pagesH } = pageHInfo;
-  const { pagesA } = pageAInfo;
-  const { pagesB } = pageBInfo;
 
   const [nameInsured, setNameI] = useState('');
   const [addressInsured, setAddressI] = useState('');
@@ -125,72 +53,6 @@ function App() {
   const [dateInsured, setDateI] = useState('');
 
   var dateFormat = moment(dateInsured).format('MM/DD/YYYY');
-
-  const handleGL = (e) => {
-    
-    // Destructuring
-    const { value, checked } = e.target;
-      
-    console.log(`${value} is ${checked}`);
-     
-    // Case 1 : The user checks the box
-    if (checked) {
-      setPageGLInfo({
-        pagesGL: [...pagesGL, value]
-      });
-    }
-  
-    // Case 2  : The user unchecks the box
-    else {
-      setPageGLInfo({
-        pagesGL: pagesGL.filter((e) => e !== value)
-      });
-    }
-  };
-
-  const handleT = (e) => {
-    
-    // Destructuring
-    const { value, checked } = e.target;
-      
-    console.log(`${value} is ${checked}`);
-     
-    // Case 1 : The user checks the box
-    if (checked) {
-      setPageTInfo({
-        pagesT: [...pagesT, value]
-      });
-    }
-  
-    // Case 2  : The user unchecks the box
-    else {
-      setPageTInfo({
-        pagesT: pagesT.filter((e) => e !== value)
-      });
-    }
-  };
-
-  const handleWC = (e) => {
-    
-    // Destructuring
-    const { value, checked } = e.target;
-      
-    console.log(`${value} is ${checked}`);
-     
-    // Case 1 : The user checks the box
-    if (checked) {
-      setPageWCInfo({
-        pagesWC: [...pagesWC, value]
-      });
-    }
-  
-    // Case 2  : The user unchecks the box
-    else {
-      setPageWCInfo({
-        pagesWC: pagesWC.filter((e) => e !== value)
-      });
-    }
-  };
 
   const handleH = (e) => {
     
@@ -214,71 +76,7 @@ function App() {
     }
   };
 
-  const handleA = (e) => {
-    
-    // Destructuring
-    const { value, checked } = e.target;
-      
-    console.log(`${value} is ${checked}`);
-     
-    // Case 1 : The user checks the box
-    if (checked) {
-      setPageAInfo({
-        pagesA: [...pagesA, value]
-      });
-    }
-  
-    // Case 2  : The user unchecks the box
-    else {
-      setPageAInfo({
-        pagesA: pagesA.filter((e) => e !== value)
-      });
-    }
-  };
-
-  const handleB = (e) => {
-    
-    // Destructuring
-    const { value, checked } = e.target;
-      
-    console.log(`${value} is ${checked}`);
-     
-    // Case 1 : The user checks the box
-    if (checked) {
-      setPageBInfo({
-        pagesB: [...pagesB, value]
-      });
-    }
-  
-    // Case 2  : The user unchecks the box
-    else {
-      setPageBInfo({
-        pagesB: pagesB.filter((e) => e !== value)
-      });
-    }
-  };
-
-  /*const handleSubmit = (e) => {
-    e.preventDefault();
-    const name = e.target.insuredName.value;
-    const initials = e.target.insuredInitials.value;
-    console.log("Insured Name: " + name);
-    console.log("Initials: " + initials);
-  }*/
-
   /* OTROS */
-
-  useEffect(() => {
-    document === "bond" ? setBondVisible(true) : setBondVisible(false);
-    document === "commercialAuto" ? setCommercialAutoVisible(true) : setCommercialAutoVisible(false);
-    document === "flood" ? setFloodVisible(true) : setFloodVisible(false);
-    document === "generalLiability" ? setGeneralLiabilityVisible(true) : setGeneralLiabilityVisible(false);
-    document === "home" ? setHomeVisible(true) : setHomeVisible(false);
-    document === "trucking" ? setTruckingVisible(true) : setTruckingVisible(false);
-    document === "workersComp" ? setWorkersCompVisible(true) : setWorkersCompVisible(false);
-    document === "specialEvent" ? setSpecialEventVisible(true) : setSpecialEventVisible(false);
-    document === "garageLiability" ? setGarageLiabilityVisible(true) : setGarageLiabilityVisible(false);
-  }, [document])
 
   const [visible, setVisible] = useState(true)
 
@@ -315,102 +113,17 @@ function App() {
           <input className="text-center" id="dateI" type="date" onChange={e => setDateI(e.target.value)}/> {console.log(dateInsured)}
         </div>
       </div>
-      <div className='d-flex justify-content-center py-2 bg-nav2'>
-        <div>
-            <label className="mx-1 fw-bold" for="doc">Document:</label> 
-            <select id="doc" value={document} onChange={handleRadioButton} className="text-center">
-                <option value="selectDocument" default>Select a Document</option>
-                <option value="bond">Bond</option>
-                <option value="commercialAuto">Commercial Auto</option>
-                <option value="flood">Flood</option>
-                <option value="generalLiability">General Liability</option>
-                <option value="home">Home</option>
-                <option value="trucking">Trucking</option>
-                <option value="workersComp">Workers' Comp</option>
-                <option value="specialEvent">Special Event</option>
-                <option value="garageLiability">Garage Liability</option>
-            </select>
-          </div>
-
-          {generalLiabilityVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesGL" onChange={handleGL}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesGL" onChange={handleGL}/> Credit Card
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesGL" onChange={handleGL}/> Spanish BF
-            </div>
-          }
-
-          {truckingVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="assignedRisk" name="pagesT" onChange={handleT}/> Assigned Risk
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesT" onChange={handleT}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesT" onChange={handleT}/> Credit Card
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesT" onChange={handleT}/> Spanish BF
-            </div>
-          }
-
-          {workersCompVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesWC" onChange={handleWC}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesWC" onChange={handleWC}/> Credit Card
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesWC" onChange={handleWC}/> Spanish BF
-            </div>
-          }
-
-          {homeVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="renters" name="pagesH" onChange={handleH}/> Renters
-              <input class="ms-2 me-1" type="checkbox" value="dp3" name="pagesH" onChange={handleH}/> DP3/HO3/Mobile
-              <input class="ms-2 me-1" type="checkbox" value="ho4" name="pagesH" onChange={handleH}/> HO4
-              <input class="ms-2 me-1" type="checkbox" value="ho6" name="pagesH" onChange={handleH}/> HO6
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesH" onChange={handleH}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesH" onChange={handleH}/> Credit Card
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesH" onChange={handleH}/> Spanish BF
-            </div>
-          }
-
-          {commercialAutoVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="infinity" name="pagesA" onChange={handleA}/> Infinity
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesA" onChange={handleA}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesA" onChange={handleA}/> Credit Card
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesA" onChange={handleA}/> Spanish BF
-            </div>
-          }
-
-          {bondVisible &&
-            <div class="d-flex align-items-center">
-              <label className="ms-2 fw-bold" for="doc">Pages:</label>
-              <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesB" onChange={handleB}/> Balance Due
-              <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesB" onChange={handleB}/> Credit Card Form
-              <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesB" onChange={handleB}/> Spanish BF
-            </div>
-          }
+      <div className='text-center py-2 bg-nav2'>
+          <label className="ms-2 fw-bold" for="doc">Pages:</label>
+          <input class="ms-2 me-1" type="checkbox" value="balanceDue" name="pagesH" onChange={handleH}/> Balance Due
+          <input class="ms-2 me-1" type="checkbox" value="creditCard" name="pagesH" onChange={handleH}/> Credit Card
+          <input class="ms-2 me-1" type="checkbox" value="spanishBF" name="pagesH" onChange={handleH}/> Spanish BF
       </div>
     </nav>
 
       <Page 
         ref = {componentRef} 
-        bond = {bondVisible} 
-        commercialAuto = {commercialAutoVisible}
-        flood = {floodVisible}
-        generalLiability = {generalLiabilityVisible}
-        home = {homeVisible}
-        trucking = {truckingVisible}
-        workersComp = {workersCompVisible}
-        specialEvent = {specialEventVisible}
-        garageLiability = {garageLiabilityVisible}
-        pageGL = {pagesGL}
-        pageT = {pagesT}
-        pageWC = {pagesWC}
         pageH = {pagesH}
-        pageA = {pagesA}
-        pageB = {pagesB}
         nameI = {nameInsured}
         addressI = {addressInsured}
         phoneI = {phoneInsured}
--- src/components/HomeInternals/Home.js
+++ src/components/HomeInternals/Home.js
@@ -3,10 +3,7 @@ import moment from 'moment/moment'
 import Badge from "../Badge.";
 import BFHomeInternals from "./pages/BFHomeInternals";
 import CCHomeInternals from "./pages/CCHomeInternals";
-import DP3HO3Mobile from "./pages/DP3HO3Mobile";
 import HO4 from "./pages/HO4";
-import HO6 from "./pages/HO6";
-import ISHome from "./pages/ISHome";
 import PAHomeInternals from "./pages/PAHomeInternals";
 import PNHomeInternals from "./pages/PNHomeInternals";
 import RentersIS from "./pages/RentersIS";
@@ -14,35 +11,18 @@ import SBFHomeInternals from "./pages/SBFHomeInternals";
 
 export default function Home({pages, name, address, phone, date}) {
 
-    let brokerFee, balanceDue, creditCard, dP3, hO4, hO6, renters, promiseA;
+    let brokerFee, balanceDue, creditCard, hO4, renters, promiseA;
     let count = 0;
 
     const [dateHO, setDateHO] = useState('')
 
     let date2 = moment(dateHO).format('MM-DD-YYYY');
 
-    if (Object.values(pages).includes("renters")) {
-        count++;
-        renters = <><Badge text={count}/><RentersIS/></>;
-    } else {
-        count++;
-        renters = <><Badge text={count}/><ISHome/></>;
-    }
+    count++;
+    renters = <><Badge text={count}/><RentersIS/></>;
 
-    if (Object.values(pages).includes("dp3")) {
-        count++;
-        dP3 = <><Badge text={count}/><DP3HO3Mobile name={name} date={date} setDateHO={setDateHO}/></>;
-    }
-
-    if (Object.values(pages).includes("ho4")) {
-        count++;
-        hO4 = <><Badge text={count}/><HO4 name={name} date={date} setDateHO={setDateHO}/></>;
-    }
-
-    if (Object.values(pages).includes("ho6")) {
-        count++;
-        hO6 = <><Badge text={count}/><HO6 name={name} date={date} setDateHO={setDateHO}/></>;
-    }
+    count++;
+    hO4 = <><Badge text={count}/><HO4 name={name} date={date} setDateHO={setDateHO}/></>;
     
     count++;
     promiseA = <><Badge text={count}/><PAHomeInternals name={name} date={date} date2={date2}/></>;
@@ -74,9 +54,7 @@ export default function Home({pages, name, address, phone, date}) {
 
             {renters}
 
-            {dP3}
             {hO4}
-            {hO6}
 
             {promiseA}
 
